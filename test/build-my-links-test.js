65b85bc55660dd73fdec6b9820a199432f2b8bf4
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-05-12 14:23:06 +0200

    Implements handling of non-exixting files

--- test/build-my-links-test.js
+++ test/build-my-links-test.js
@@ -5,9 +5,12 @@ var buildMyLinks = require('../lib/build-my-links')
 var administrasjonen = require('../data/administrasjonen.json')
 var skole = require('../data/skole.json')
 var tannhelse = require('../data/tannhelse.json')
+var tullebuff = []
 
 tap.equal(JSON.stringify(administrasjonen), JSON.stringify(buildMyLinks(['administrasjonen'])), 'It returns administrasjonen correct')
 
 tap.equal(JSON.stringify(skole), JSON.stringify(buildMyLinks(['skole'])), 'It returns skole correct')
 
 tap.equal(JSON.stringify(tannhelse), JSON.stringify(buildMyLinks(['tannhelse'])), 'It returns tannhelse correct')
+
+tap.equal(JSON.stringify(tullebuff), JSON.stringify(buildMyLinks(['tullebuff'])), 'It returns tullebuff correct')
